589d2e4e9987424e204ddf5a5ac8f4ebedc9ce1b
Bravo <ebenezerolushola8@gmail.com>
2021-03-25 10:53:45 +0100

    first commit

---
+++ src/InfoBox.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+function InfoBox({title, cases, total, countryPop}) {
+    return (
+        <Card className="infoBox">
+            <CardContent>
+                <Typography
+                    className="infoBox__title"
+                    color="textSecondary">
+                    {title}
+                </Typography>
+                <h2 className="infoBox__cases">{cases}</h2>
+                <Typography
+                    className="infoBox__total"
+                    color="textSecondary">{total} Total</Typography>
+                 <Typography
+                    className="infoBox__population"
+                    color="textSecondary">{countryPop}
+                </Typography>
+
+            </CardContent>
+
+            
+        </Card>
+    )
+}
+
+export default InfoBox;
